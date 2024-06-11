d7f28e8063cfdb5319f5ae885b9e745dd1db85c3
kryparnold <emirhanpgt@gmail.com>
2024-06-11 19:37:42 +0300

    styling changes

--- src/components/util/change-theme.tsx
+++ src/components/util/change-theme.tsx
@@ -8,7 +8,7 @@ export default function ChangeTheme() {
     const { setTheme, resolvedTheme } = useTheme();
 
     return (
-        <div className="absolute right-0 top-0 *:size-10 *:cursor-pointer aspect-square p-4">
+        <div className="absolute right-0 top-0 *:size-9 *:cursor-pointer aspect-square p-4">
             {resolvedTheme === "dark" ? (
                 <Moon onClick={() => setTheme("light")} />
             ) : (
