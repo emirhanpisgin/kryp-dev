833358cd8c74c99dc4e2c4266ab8cebc9af3cf24
kryparnold <emirhanpgt@gmail.com>
2024-06-11 07:33:16 +0300

    styling changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import TypeWriter from "@/components/typewriter";
+
 export default function Home() {
     return (
-        <main className="grid h-screen place-items-center p-4 text-3xl">
-            Kryp Arnold
+        <main className="grid h-screen place-items-center">
+            <TypeWriter
+                text="Kryp Arnold"
+                duration={0.8}
+                className="text-7xl animate-land-in [--land-delay:1000ms]"
+            />
         </main>
     );
 }
